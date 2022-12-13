3b0144af6937d71a665a4ae4873e1fdec2afc9b7
jully0504170 <benzakayyael@gmail.com>
2022-12-14 00:21:33 +0200

    bbb

--- server/routers/allergicForUserRouter.js
+++ server/routers/allergicForUserRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const allergicForUserRouter = express.Router();
 const {promiseQuery}=require('../sqlConnection');
-
+const b=5;
 module.exports = allergicForUserRouter;
